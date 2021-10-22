02b55f5090f7ffb4c96639ca9a58a5417b7660f3
Yuning Shi <shiyuning@gmail.com>
2021-10-22 15:09:05 -0400

    :tada:

---
+++ bin/cycles/cycles-wrapper.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python3
+"""Cycles Executor."""
+
+import argparse
+import csv
+import logging
+import os
+import shutil
+import subprocess
+import sys
+from string import Template
+
+log = logging.getLogger()
+
+global basedir
+basedir = os.path.dirname(__file__)
+
+def _generate_inputs(
+        prefix,
+        start_year,
+        end_year,
+        baseline,
+        crop,
+        start_planting_date,
+        end_planting_date,
+        fertilizer_rate,
+        weed_fraction,
+        forcing,
+        weather_file,
+        reinit_file,
+        crop_file,
+        soil_file,
+        **kwargs):
+
+    global basedir
+    ctrl_file = "cycles-run.ctrl"
+    op_file = "cycles-run.operation"
+
+    # process CTRL file
+    with open(f"{basedir}/template.ctrl") as t_ctrl_file:
+        src = Template(t_ctrl_file.read())
+        ctrl_data = {
+            "start_year": start_year,
+            "end_year": end_year,
+            "rotation_size": 1,
+            "crop_file": crop_file,
+            "operation_file": op_file,
+            "soil_file": soil_file,
+            "weather_file": weather_file,
+            "reinit": 0 if baseline == "True" else 1,
+        }
+        result = src.substitute(ctrl_data)
+        with open("./input/" + ctrl_file, "w") as f:
+            f.write(result)
+
+    # process Operation file
+    operation_contents = ""
+    with open(f"{basedir}/template.operation") as t_op_file:
+        src = Template(t_op_file.read())
+        op_data = {
+            "year_count": 1,
+            "crop_name": crop,
+            "fertilization_date": int(start_planting_date) - 10,
+            "fertilization_rate": fertilizer_rate,
+            "start_planting_date": start_planting_date,
+            "end_planting_date": end_planting_date,
+            "tillage_date": int(start_planting_date) + 20,
+        }
+        result = src.substitute(op_data)
+        operation_contents += result + "\n"
+
+        # handling weeds
+        if float(weed_fraction) > 0:
+            with open(f"{basedir}/template-weed.operation") as t_wd_file:
+                wd_src = Template(t_wd_file.read())
+                wd_data = {
+                    "year_count": 1,
+                    "weed_planting_date": int(start_planting_date) + 7,
+                    "weed_fraction": weed_fraction
+                }
+                wd_result = wd_src.substitute(wd_data)
+                operation_contents += wd_result + "\n"
+
+    # writing operations file
+    with open("./input/" + op_file, "w") as f:
+        f.write(operation_contents)
+
+
+def _launch(prefix, baseline, **kwargs):
+    cmd = f"{basedir}/Cycles -s -l 1 cycles-run" if baseline == "True" else f"{basedir}/Cycles cycles-run"
+    print(cmd)
+    try:
+        output = subprocess.check_output(
+            cmd, stderr=subprocess.STDOUT, shell=True, universal_newlines=True)
+    except subprocess.CalledProcessError as exc:
+        print("Status : FAIL", exc.returncode, exc.output)
+        exit(1)
+    else:
+        print("Output: \n{}\n".format(output))
+
+
+def _main():
+    parser = argparse.ArgumentParser(
+        description="Cycles executor."
+    )
+    parser.add_argument("--start-year", dest="start_year", default=2000, help="Simulation start year")
+    parser.add_argument("--end-year", dest="end_year", default=2017, help="Simulation end year")
+    parser.add_argument("-b", "--baseline", dest="baseline", default=False, help="Whether this is a baseline execution")
+    parser.add_argument("-c", "--crop", dest="crop", default="Maize", help="Crop name")
+    parser.add_argument("-s", "--start-planting-date", dest="start_planting_date", default=100, help="Start planting date")
+    parser.add_argument("-e", "--end-planting-date", dest="end_planting_date", default=0, help="End planting date") # Changed default to 0, which changes to -999 below, and implies automatic end planting date
+    parser.add_argument("-n", "--fertilizer-rate", dest="fertilizer_rate", default=0.00, help="Fertilizer rate")
+    parser.add_argument("-w", "--weed-fraction", dest="weed_fraction", default=0.0, help="Weed fraction")
+    parser.add_argument("-f", "--forcing", dest="forcing", default=False, help="Whether it uses forcing data from PIHM")
+    parser.add_argument("-l", "--weather-file", dest="weather_file", default=None, help="Weather file")
+    parser.add_argument("-r", "--reinit-file", dest="reinit_file", default=None, help="Cycles reinitialization file")
+    parser.add_argument("crop_file", help="crops file")
+    parser.add_argument("soil_file", help="Soil file")
+    args = parser.parse_args()
+
+    # setting prefix
+    prefix = "_baseline" if args.baseline == "True" else ""
+
+    if args.end_planting_date == 0:
+        args.end_planting_date = -999
+
+    _generate_inputs(prefix, **vars(args))
+    _launch(prefix, **vars(args))
+
+
+if __name__ == "__main__":
+    _main()
---
+++ cycles_dojo.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python3
+
+import json
+import pandas as pd
+import numpy as np
+import re
+import os
+import argparse
+
+import subprocess
+
+RESOURCES_FILE = "data/HOAResources.csv"
+CROPLAND_FILE = "data/HOACropland.csv"
+CYCLES_RUN_SCRIPT = "bin/cycles/run"
+TMP_DIR = "tmp"
+OUTPUT_FILE = "outputs/cycles_results.csv"
+CROPS_FILE = "data/crops-horn-of-africa.crop"
+SOIL_WEATHER_DIR = "data/soil_weather"
+
+def run_cycles(params):
+    cropland_df = pd.read_csv(CROPLAND_FILE)
+    cropland_df.set_index(['country', 'admin1', 'admin2', 'admin3'], inplace=True)
+    
+    df = pd.read_csv(RESOURCES_FILE)
+    df.set_index(['country', 'admin1', 'admin2', 'admin3'], inplace=True)
+    country_soil_points = df.loc[params["country"]]
+    
+    # Run Cycles for all soil points in the country
+    # - TODO: Should run in parallel
+    first = True
+    for index, point in country_soil_points.iterrows():
+        
+        # Get the crop fractional area for this region
+        cropland_row = cropland_df.loc[params["country"], index[0], index[1], index[2]]
+        crop_fractional_area = cropland_row[params["crop_name"].lower()+"_fractional_area"]
+        
+        # Get the input/output files
+        inputfile = point["filename"]        
+        season_file = f"{TMP_DIR}/{inputfile}.season"
+        summary_file = f"{TMP_DIR}/{inputfile}.summary"        
+
+        # ** Run Cycles **
+        cmd = [CYCLES_RUN_SCRIPT, 
+               '-i1', f"{SOIL_WEATHER_DIR}/{inputfile}",
+               '-i2', CROPS_FILE,
+               '-o1', season_file,
+               '-o2', summary_file,
+               '-p1', params["start_year"],
+               '-p2', params["end_year"],
+               '-p3', params["crop_name"],
+               '-p4', params["start_planting_day"],
+               '-p5', params["fertilizer_rate"],
+               '-p6', params["weed_fraction"],
+               '-p7', "False"
+              ]
+        print(cmd)
+        try:
+              output = subprocess.run(cmd)
+        except subprocess.CalledProcessError as exc:
+              print("Status : FAIL", exc.returncode, exc.output)
+              continue
+        
+        # Load the output file
+        exdf = get_dataframe_for_execution_result(season_file, index, params, 
+                                                ["grain_yield", "cum._n_stress", "actual_tr", "potential_tr"])
+
+        # Filter/Modify/Add Columns
+        exdf["crop_production"] = exdf["grain_yield"]*crop_fractional_area
+        exdf["water_stress"] = 1 - exdf["actual_tr"]/exdf["potential_tr"]
+        exdf = exdf.rename(columns={'cum._n_stress': 'nitrogen_stress'})
+        exdf = exdf.drop(['actual_tr', 'potential_tr'], axis = 1)
+        
+        # Write output
+        if first:
+            exdf.to_csv(OUTPUT_FILE, index=False)
+            first = False  
+        else:
+            exdf.to_csv(OUTPUT_FILE, mode='a', header=False, index=False)
+
+
+def load_execution_result(outputloc):
+    df = pd.read_csv(outputloc, sep='\t', header=0, skiprows=[1], skipinitialspace=True)
+    df = df.rename(columns=lambda x: x.strip().lower().replace(' ', '_'))
+    df['date'] = pd.to_datetime(df['date'], format="%Y-%m-%d")
+    return df
+
+
+def get_dataframe_for_execution_result(season_file, index, params, result_fields):
+    df = load_execution_result(season_file)
+
+    # Fetch feature values
+    ndf = df.filter(result_fields, axis=1)
+
+    # Insert timestamp
+    ndf.insert(0, "date", df['date']) #.values.astype(np.int64) // 10 ** 9)
+
+    # Insert geospatial details
+    ndf.insert(1, "country", params["country"])
+    ndf.insert(2, "admin1", index[0] if index[0] else None)
+    ndf.insert(3, "admin2", index[1] if index[1] else None)
+    ndf.insert(4, "admin3", index[2] if index[2] else None)
+
+    return ndf
+
+
+def _main():
+    parser = argparse.ArgumentParser(
+        description="Cycles execution for a country"
+    )
+    parser.add_argument("--country", dest="country", default="Kenya", help="Crop name")    
+    parser.add_argument("--crop-name", dest="crop_name", default="Maize", help="Crop name")
+    parser.add_argument("--start-year", dest="start_year", default=2000, help="Simulation start year")
+    parser.add_argument("--end-year", dest="end_year", default=2020, help="Simulation end year")    
+    parser.add_argument("--start-planting-day", dest="start_planting_day", default=103, help="Start planting date")
+    parser.add_argument("--fertilizer-rate", dest="fertilizer_rate", default=50.00, help="Fertilizer rate")
+    parser.add_argument("--weed-fraction", dest="weed_fraction", default=0.2, help="Weed fraction")
+    args = parser.parse_args()
+    run_cycles(vars(args))
+
+
+if __name__ == "__main__":
+    _main()
